35823ce9d91ac68b20c1c0b8970e438345f9f0d6
Amit0412 <59620280+Amit0412@users.noreply.github.com>
2021-01-31 14:25:22 +0530

    switch statement

---
+++ switch.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args)
+    {
+        int day=1;
+        switch(day){
+            case 1:
+                System.out.println("First option");
+                break;
+            case 2:
+                System.out.println("Second option");
+                break;
+        }
+    }
+}
