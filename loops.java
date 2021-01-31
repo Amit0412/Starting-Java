302bd8ed9c926a97204bcf7914a0f6a3b5f74dfb
Amit0412 <59620280+Amit0412@users.noreply.github.com>
2021-01-31 14:09:52 +0530

    loops

---
+++ loops.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void main(String[] args)
+    {
+        int r=3;
+        while(r<=7)
+        {
+            System.out.println(r);
+            r=r+1;
+        }
+    }
+}
+
+
+public class Main{
+    public static void main(String[] args){
+        int r;
+        for(r=0;r<7;r++)
+        {
+            System.out.println(r);
+        }
+    }
+}
