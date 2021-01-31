a5150416f6a4e05c45117e69944db8395c94a0f2
Amit0412 <59620280+Amit0412@users.noreply.github.com>
2021-01-31 13:54:16 +0530

    IF-ELSE

---
+++ Conditional.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args)
+    {
+        int r=33;
+        if(r==3)
+        {
+            System.out.println("Number matched");
+        }
+        else
+        {
+            System.out.println("error in matching");
+        }
+    }
+}
