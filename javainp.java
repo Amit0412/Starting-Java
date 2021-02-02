6a32d09b50ab9d6187aae407f1805dbc0c1460fa
Amit0412 <59620280+Amit0412@users.noreply.github.com>
2021-02-02 10:07:15 +0530

    Input

---
+++ javainp.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class Main{
+    public static void main(String args[])
+    {
+        Scanner my=new Scanner(System.in);
+        int n=my.nextInt();
+        int m=my.nextInt();
+        System.out.println(Math.sqrt(n+m));
+    }
+}
